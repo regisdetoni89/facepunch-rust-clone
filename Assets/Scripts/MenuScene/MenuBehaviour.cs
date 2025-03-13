8aa93299eb918925a42309b277d5d00690d132ae
Regis de Toni <regis@mura.com.br>
2025-03-13 06:16:43 -0300

    added menu behaviour add icon for facepunch servers

--- Assets/Scripts/MenuScene/MenuBehaviour.cs
+++ Assets/Scripts/MenuScene/MenuBehaviour.cs
@@ -3,6 +3,14 @@ using UnityEngine;
 public class MenuBehaviour : MonoBehaviour
 {
     
+    public GameObject playGameRightMenu;
+
+    public void PlayGame(){
+        if(playGameRightMenu.activeSelf)
+            playGameRightMenu.SetActive(false);
+        else    
+            playGameRightMenu.SetActive(true);
+    }
 
     public void QuitGame(){
         #if UNITY_EDITOR
