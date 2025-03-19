23a2a195f383906da620f38acf4f5afc107557c1
Regis de Toni <regis@mura.com.br>
2025-03-19 01:00:04 -0300

    improve the menu

--- Assets/Scripts/MenuScene/MenuBehaviour.cs
+++ Assets/Scripts/MenuScene/MenuBehaviour.cs
@@ -3,13 +3,36 @@ using UnityEngine;
 public class MenuBehaviour : MonoBehaviour
 {
     
-    public GameObject playGameRightMenu;
+    public GameObject[] rightMenus;
 
-    public void PlayGame(){
-        if(playGameRightMenu.activeSelf)
-            playGameRightMenu.SetActive(false);
-        else    
-            playGameRightMenu.SetActive(true);
+    public void enableRightMenu(GameObject rightMenu){
+
+        if(!isARightMenu(rightMenu)){
+            Debug.LogWarning("Trying to enable a menu that is not in the right menus list.");
+            return;
+        }
+
+        if(rightMenu.activeSelf){
+            disableAllRightMenus();
+        }else{
+            disableAllRightMenus();
+            rightMenu.SetActive(true);
+        }
+    }
+
+    private bool isARightMenu(GameObject menu){
+        for(int i = 0; i < rightMenus.Length; i++){
+            if(rightMenus[i] == menu){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void disableAllRightMenus(){
+        for(int i = 0; i < rightMenus.Length; i++){
+            rightMenus[i].SetActive(false);
+        }
     }
 
     public void QuitGame(){
