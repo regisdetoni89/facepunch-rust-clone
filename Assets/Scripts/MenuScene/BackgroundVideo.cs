c63d6a27440e58c06c4d055f0ad815451b07ade1
Regis de Toni <regis@mura.com.br>
2025-03-19 00:48:41 -0300

    fix for background video

--- Assets/Scripts/MenuScene/BackgroundVideo.cs
+++ Assets/Scripts/MenuScene/BackgroundVideo.cs
@@ -8,14 +8,11 @@ public class BackgroundVideo : MonoBehaviour
     
     void Start()
     {
-        VideoPlayer videoPlayer = gameObject.AddComponent<VideoPlayer>();
+        VideoPlayer videoPlayer = gameObject.GetComponent<VideoPlayer>();
         videoPlayer.playOnAwake = true;
         videoPlayer.isLooping = true;
         videoPlayer.renderMode = VideoRenderMode.CameraNearPlane;
         videoPlayer.targetCamera = Camera.main;
-        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
-        videoPlayer.EnableAudioTrack(0, true);
-        videoPlayer.SetTargetAudioSource(0, gameObject.AddComponent<AudioSource>());
         videoPlayer.clip = videoClips[Random.Range(0, videoClips.Length)];
         videoPlayer.Play();
     }
