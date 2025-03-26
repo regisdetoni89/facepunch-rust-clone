b7bedd5c54c3af6028d268da81aa0ee0c652e6bc
Regis de Toni <regis@mura.com.br>
2025-03-26 10:34:12 -0300

    fullscreen popup of server information

--- Assets/Scripts/MenuScene/ServerPopupInformation.cs
+++ Assets/Scripts/MenuScene/ServerPopupInformation.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class ServerPopupInformation : MonoBehaviour
 {
-    
+
     public void SetInformation(string serverName, string serverIP, string serverMap, int currentPlayers, int maxPlayers, string wipeAgo)
     {
-        transform.Find("ServerName").GetComponent<TMPro.TextMeshProUGUI>().text = serverName;
-        transform.Find("ServerIP").GetComponent<TMPro.TextMeshProUGUI>().text = serverIP;
-        transform.Find("ServerMap").GetComponent<TMPro.TextMeshProUGUI>().text = serverMap;
-        transform.Find("ServerPlayers").GetComponent<TMPro.TextMeshProUGUI>().text = currentPlayers + "/" + maxPlayers;
-        transform.Find("ServerWipeAgo").GetComponent<TMPro.TextMeshProUGUI>().text = wipeAgo;
+        // transform.Find("ServerName").GetComponent<TMPro.TextMeshProUGUI>().text = serverName;
+        // transform.Find("ServerIP").GetComponent<TMPro.TextMeshProUGUI>().text = serverIP;
+        // transform.Find("ServerMap").GetComponent<TMPro.TextMeshProUGUI>().text = serverMap;
+        // transform.Find("ServerPlayers").GetComponent<TMPro.TextMeshProUGUI>().text = currentPlayers + "/" + maxPlayers;
+        // transform.Find("ServerWipeAgo").GetComponent<TMPro.TextMeshProUGUI>().text = wipeAgo;
     }
 }
