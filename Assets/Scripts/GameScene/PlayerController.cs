5fd2b9ff6bd870f0f3aa75ee98ca13d278fce836
Regis de Toni <regis@mura.com.br>
2025-03-26 15:39:29 -0300

    Add slide capability and hability to jump if slow enough

--- Assets/Scripts/GameScene/PlayerController.cs
+++ Assets/Scripts/GameScene/PlayerController.cs
@@ -8,20 +8,49 @@ public class PlayerController : MonoBehaviour{
     public float jumpStrength = 5f;
     private float verticalSpeed = 0f;
 
+    public float slideFriction = 0.3f;
+
+    private Vector3 hitNormal = Vector3.down;
+
+    private Vector3 vel = Vector3.zero;
+
+    void OnControllerColliderHit (ControllerColliderHit hit) {
+	    hitNormal = hit.normal;
+        verticalSpeed = 0;
+    }
+
     void Update()
     {
+
+        bool isStuck = controller.velocity.magnitude <= 1.3f && controller.isGrounded;
+        bool isGrounded = Vector3.Angle (Vector3.up, hitNormal) <= controller.slopeLimit;
+
         transform.Rotate(0, Input.GetAxis("Mouse X") * turnSpeed * Time.deltaTime, 0);
-        Vector3 vel = transform.forward * Input.GetAxis("Vertical") * speed;
-        vel+= transform.right * Input.GetAxis("Horizontal") * speed;
-        if (controller.isGrounded){
-            verticalSpeed = 0;
+        if (isGrounded){
+            vel = transform.forward * Input.GetAxis("Vertical") * speed;
+            vel += transform.right * Input.GetAxis("Horizontal") * speed;
+        }else{
+            vel += transform.forward * Input.GetAxis("Vertical") * speed * 0.5f * Time.deltaTime;
+            vel += transform.right * Input.GetAxis("Horizontal") * speed * 0.5f * Time.deltaTime;
+            if(controller.isGrounded){
+                vel.x += (1f - hitNormal.y) * hitNormal.x * (1f - slideFriction);
+                vel.z += (1f - hitNormal.y) * hitNormal.z * (1f - slideFriction);
+            }
+        }
+        if (isGrounded || isStuck){
             if (Input.GetAxis("Jump") != 0){
                 verticalSpeed = jumpStrength;
+                if(isStuck){
+                    vel = Vector3.zero;
+                }
+                hitNormal = Vector3.down;
             }
         }
 
         verticalSpeed -= 9.8f * Time.deltaTime;
         vel.y = verticalSpeed;
+
+
 
         controller.Move(vel * Time.deltaTime);
     }
